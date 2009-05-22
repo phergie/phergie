47d4c06e2a5d9046afb74f715e15ba2040fc4c70
Eli White <eli@eliw.com>
2009-05-22 01:57:54 +0000

    Converted Daddy plugin from v1.0 to v2.0

---
+++ Phergie/Plugin/Daddy.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Php plugin for Phergie.
+ *
+ * Simply responds to messages addressed to the bot that contain the phrase
+ * "Who's your daddy?"
+ *
+ */
+class Phergie_Plugin_Daddy extends Phergie_Plugin_Abstract
+{
+    /**
+     * Checks messages for the question to which it should respond and sends a
+     * response when appropriate
+     *
+     * @return void
+     */
+    public function onPrivmsg()
+    {
+        $bot = $this->_config['command.prefix'];
+        $text = $this->_event->getArgument(1);
+        $target = $this->_event->getNick();
+        if (preg_match('/' . preg_quote($bot) . 
+                    '\s*?who\'?s y(?:our|a) ([^?]+)\??/iAD', $text, $m)) {
+            if ($this->_config['daddy.curses'] && mt_rand(0, 5) === 5) {
+                $this->doPrivmsg($this->_event->getSource(), $target . 
+                    ': I am your ' . $m[1] . ', bitch!');
+            } else {
+                $this->doPrivmsg($this->_event->getSource(), 'You\'re my ' . $m[1] . 
+                    ', ' . $target . '!');
+            }
+        }
+    }
+
+}
