441cd6b3cf912b3251a9034a4080b4095ade436f
Sean Coates <sean@caedmon.net>
2010-01-30 06:45:47 +0000

    the logic for determining the current directory name was wrong; it assumed that the current directory name is 'Phergie', but in my case, the phergie1 code was in ../Phergie/ so Bad Stuff happened

--- Phergie/Autoload.php
+++ Phergie/Autoload.php
@@ -12,7 +12,7 @@ class Phergie_Autoload
      */
     public function __construct()
     {
-        $path = dirname(dirname(__FILE__));
+        $path = dirname(__FILE__);
         $includePath = get_include_path();
         $includePathList = explode(PATH_SEPARATOR, $includePath); 
         if (!in_array($path, $includePathList)) {
@@ -28,6 +28,13 @@ class Phergie_Autoload
      */
     public function load($class)
     {
+        static $preflen = null;
+        if (null === $preflen) {
+            $preflen = strlen('Phergie_');
+        }
+        if (substr($class, 0, $preflen)) {
+            $class = substr($class, $preflen);
+        }
         include str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
     }
 
--- Phergie/Settings.php
+++ Phergie/Settings.php
@@ -6,9 +6,9 @@ return array(
         array(
             'host' => 'irc.freenode.net',
             'port' => 6667,
-            'username' => 'Elazar',
-            'realname' => 'Matthew Turland',
-            'nick' => 'Phergie2',
+            'username' => 'brewbot2',
+            'realname' => 'brewbot2',
+            'nick' => 'brewbot2',
             'ssl' => false
         )
     ),
@@ -30,15 +30,15 @@ return array(
     'plugins.autoload' => true,
 
     'altnick.nicks' => array(
-        'Phergie2_'
+        'brewbot2_'
     ),
 
-    'autojoin.channels' => '#phergie',
+    'autojoin.channels' => '#seantest',
 
     'ping.event' => 600,
 
     'ping.ping' => 10,
 
-    'command.prefix' => 'Phergie2: '
+    'command.prefix' => '!'
 
 );
