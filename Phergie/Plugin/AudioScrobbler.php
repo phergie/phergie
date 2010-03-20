741297a39a4ac1ff7bf041585faf61bb4c91d0ca
elazar <tobias382@gmail.com>
2010-03-20 12:35:17 -0500

    Added AudioScrobbler plugin (thanks MattRead)

---
+++ Phergie/Plugin/AudioScrobbler.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Phergie
+ *
+ * PHP version 5
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://phergie.org/license
+ *
+ * @category  Phergie
+ * @package   Phergie_Plugin_AudioScrobbler
+ * @author    Phergie Development Team <team@phergie.org>
+ * @copyright 2008-2010 Phergie Development Team (http://phergie.org)
+ * @license   http://phergie.org/license New BSD License
+ * @link      http://pear.phergie.org/package/Phergie_Plugin_AudioScrobbler
+ */
+
+/**
+ * Uses a self CTCP PING to ensure that the client connection has not been
+ * dropped.
+ *
+ * @category Phergie
+ * @package  Phergie_Plugin_AudioScrobbler
+ * @author   Phergie Development Team <team@phergie.org>
+ * @license  http://phergie.org/license New BSD License
+ * @link     http://pear.phergie.org/package/Phergie_Plugin_AudioScrobbler
+ * @uses     Phergie_Plugin_Command pear.phergie.org
+ * @uses     extension simplexml
+ */
+class Phergie_Plugin_AudioScrobbler extends Phergie_Plugin_Abstract
+{
+    /**
+     * Last.FM API entry point
+     *
+     * @var string
+     */
+    protected $lastfmUrl = 'http://ws.audioscrobbler.com/2.0/';
+    
+    /**
+     * Libre.FM API entry point
+     *
+     * @var string
+     */
+    protected $librefmUrl = 'http://alpha.dev.libre.fm/2.0/';
+    
+    /**
+     * Scrobbler query string for user.getRecentTracks
+     *
+     * @var string
+     */
+    protected $query = '?method=user.getrecenttracks&user=%s&api_key=%s';
+    
+    /**
+     * check and load plugin's dependencies.
+     *
+     * @return void
+     */
+    public function onLoad()
+    {
+        if (!extension_loaded('simplexml')) {
+            $this->fail('SimpleXML php extension is required');
+        }
+        
+        $this->getPluginHandler()->getPlugin('Command');
+    }
+    
+    /**
+     * Command function to get user's status on last.fm
+     * 
+     * @param string $user User identifier
+     *
+     * @return void
+     */
+    public function onCommandLastfm($user = null)
+    {
+        if ($key = $this->config['audioscrobbler.lastfm_api_key']) {
+            $scrobbled = $this->getScrobbled($user, $this->lastfm_url, $key);
+            $this->doPrivmsg($this->getEvent()->getSource(), $scrobbled);
+        }
+    }
+
+    /**
+     * Command function to get users status on libre.fm
+     * 
+     * @param string $user User identifier
+     *
+     * @return void
+     */
+    public function onCommandLibrefm($user = null)
+    {
+        if ($key = $this->config['audioscrobbler.librefm_api_key']) {
+            $scrobbled = $this->getScrobbled($user, $this->librefm_url, $key);
+            $this->doPrivmsg($this->getEvent()->getSource(), $scrobbled);
+        }
+    }
+
+    /**
+     * Simple Scrobbler API function to get recent track formatted in string
+     * 
+     * @param string $user User name to lookup
+     * @param string $url  Base URL of the scrobbler service
+     * @param string $key  Scrobbler service API key
+     *
+     * @return string A formatted string of the most recent track played.
+     */
+    public function getScrobbled($user, $url, $key)
+    {
+        $user = $user ? $user : $this->getEvent()->getNick();
+        $url = sprintf($url . $this->query, urlencode($user), urlencode($key));
+        
+        $response = file_get_contents($url);
+        try {
+            $xml = new SimpleXMLElement($response);
+        }
+        catch (Exception $e) {
+            return 'Can\'t find status for ' . $user;
+        }
+        
+        if ($xml->error) {
+            return 'Can\'t find status for ' . $user;
+        }
+        
+        $recenttracks = $xml->recenttracks;
+        $track = $recenttracks->track[0];
+        if (isset($track['nowplaying'])) {
+            $msg = sprintf(
+                '%s is listening to %s by %s',
+                $recenttracks['user'],
+                $track->name,
+                $track->artist
+            );
+        } else {
+            $msg = sprintf(
+                '%s, %s was listening to %s by %s',
+                $track->date,
+                $recenttracks['user'],
+                $track->name,
+                $track->artist
+            );
+        }
+        if ($track->streamable == 1) {
+            $msg .= ' - ' . $track->url;
+        }
+        return $msg;
+    }
+}
