e360e4079bf1215b70f4e255416d8090f5ff3a79
elazar <tobias382@gmail.com>
2010-05-18 17:20:27 -0500

    Fixed spacing in the file and class docblocks of Phergie_Plugin_Cache

--- Phergie/Plugin/Cache.php
+++ Phergie/Plugin/Cache.php
@@ -1,33 +1,33 @@
 <?php
 /**
-* Phergie
-*
-* PHP version 5
-*
-* LICENSE
-*
-* This source file is subject to the new BSD license that is bundled
-* with this package in the file LICENSE.
-* It is also available through the world-wide-web at this URL:
-* http://phergie.org/license
-*
-* @category  Phergie
-* @package   Phergie_Plugin_Cache
-* @author    Phergie Development Team <team@phergie.org>
-* @copyright 2008-2010 Phergie Development Team (http://phergie.org)
-* @license   http://phergie.org/license New BSD License
-* @link      http://pear.phergie.org/package/Phergie_Plugin_Cache
-*/
+ * Phergie
+ *
+ * PHP version 5
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://phergie.org/license
+ *
+ * @category  Phergie
+ * @package   Phergie_Plugin_Cache
+ * @author    Phergie Development Team <team@phergie.org>
+ * @copyright 2008-2010 Phergie Development Team (http://phergie.org)
+ * @license   http://phergie.org/license New BSD License
+ * @link      http://pear.phergie.org/package/Phergie_Plugin_Cache
+ */
 
 /**
-* Implements a generic cache to be used by other plugins.
-*
-* @category Phergie
-* @package  Phergie_Plugin_Cache
-* @author   Phergie Development Team <team@phergie.org>
-* @license  http://phergie.org/license New BSD License
-* @link     http://pear.phergie.org/package/Phergie_Plugin_Cache
-*/
+ * Implements a generic cache to be used by other plugins.
+ *
+ * @category Phergie
+ * @package  Phergie_Plugin_Cache
+ * @author   Phergie Development Team <team@phergie.org>
+ * @license  http://phergie.org/license New BSD License
+ * @link     http://pear.phergie.org/package/Phergie_Plugin_Cache
+ */
 class Phergie_Plugin_Cache extends Phergie_Plugin_Abstract
 {
     /**
