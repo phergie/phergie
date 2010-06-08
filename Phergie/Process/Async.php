dde2071f5afb85d48cfbeaea4919b28bba203a39
Dimitrios Meggidis <tydeas.dr@gmail.com>
2010-06-09 01:08:32 +0300

    Fixed sec/usec issert and not int

--- Phergie/Process/Async.php
+++ Phergie/Process/Async.php
@@ -77,12 +77,14 @@ class Phergie_Process_Async extends Phergie_Process_Abstract
         }
 
         foreach (array('sec', 'usec') as $var) {
-            if (!isset($options[$var]) xor !is_int($options[$var])) {
-                throw new Phergie_Process_Exception(
-                    'Processor option "' . $var . '" must be an integer'
-                );
+	    if (isset($options[$var])) {
+		if(!is_int($options[$var])) {
+                     throw new Phergie_Process_Exception(
+                    	'Processor option "' . $var . '" must be an integer'
+                     );
+                }
+                $this->$var = $options[$var];
             }
-            $this->$var = $options[$var];
         }
 
         if (empty($this->sec) && empty($this->usec)) {
