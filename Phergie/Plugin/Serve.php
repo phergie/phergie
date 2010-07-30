2d5aa457b54ebd30ba55838a27701bac02ecce4c
elazar <tobias382@gmail.com>
2010-07-30 17:50:47 -0500

    Modified the Serve plugin to make Censor plugin integration optional

--- Phergie/Plugin/Serve.php
+++ Phergie/Plugin/Serve.php
@@ -113,20 +113,21 @@ class Phergie_Plugin_Serve extends Phergie_Plugin_Abstract
         $item = $this->getItem($database, $table, $match);
 
         // Reprocess the request for censorship if required
-        $attempts = 0;
-        while ($censor && $attempts < 3) {
+        if ($this->plugins->hasPlugin('Censor')) {
             $plugin = $this->plugins->getPlugin('Censor');
-            $clean = $plugin->cleanString($item->name);
-            if ($item->name != $clean) {
-                $attempts++;
-                $item = $this->getItem($database, $table, $match);
-            } else {
-                $censor = false;
+            $attempts = 0;
+            while ($censor && $attempts < 3) {
+                $clean = $plugin->cleanString($item->name);
+                if ($item->name != $clean) {
+                    $attempts++;
+                    $item = $this->getItem($database, $table, $match);
+                } else {
+                    $censor = false;
+                }
             }
-        }
-
-        if ($censor && $attempts == 3) {
-            $this->doAction($this->event->getSource(), 'shrugs.');
+            if ($censor && $attempts == 3) {
+                $this->doAction($this->event->getSource(), 'shrugs.');
+            }
         }
 
         // Derive the proper article for the item
