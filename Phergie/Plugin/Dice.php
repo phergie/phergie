b78ab8eaac80f5c3fc775208a3b0ba93a0d9b8a4
Dossy Shiobara <dossy@panoptic.com>
2011-05-30 18:45:59 -0400

    Simulated die rolls are now O(1) insted of O(N).

--- Phergie/Plugin/Dice.php
+++ Phergie/Plugin/Dice.php
@@ -58,10 +58,8 @@ class Phergie_Plugin_Dice extends Phergie_Plugin_Abstract
     {
         if (preg_match('/(\d+)\s*d\s*(\d+)(\s*[-+]\s*\d+)?(.*)/i', $message, $matches)) {
             list (, $num, $die, $mod, $rest) = $matches;
-            $roll = 0;
-            for ($i = 0; $i < $num; $i++) {
-                $roll += mt_rand(1, $die);
-            }
+            $roll = $num;
+            $roll += mt_rand(0, $num * ($die - 1));
             if (!empty($mod)) {
                 $roll += intval(preg_replace('/\s+/', '', $mod));
             }
