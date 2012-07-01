6c574b3afccc78ad9ac3ce51cbd1dd7aafa88bf0
elazar <tobias382@gmail.com>
2012-06-30 22:50:19 -0500

    Made coding standard and other corrections to pull request 223

--- Phergie/Plugin/PingPong.php
+++ Phergie/Plugin/PingPong.php
@@ -12,51 +12,46 @@
  * http://phergie.org/license
  *
  * @category  Phergie
- * @package   Phergie_Plugin_Pong
+ * @package   Phergie_Plugin_PingPong
  * @author    Phergie Development Team <team@phergie.org>
  * @copyright 2008-2011 Phergie Development Team (http://phergie.org)
  * @license   http://phergie.org/license New BSD License
- * @link      http://pear.phergie.org/package/Phergie_Plugin_Pong
+ * @link      http://pear.phergie.org/package/Phergie_Plugin_PingPong
  */
 
 /**
- * Responds with "$nick: pong" when some wrote "ping".
- * Additionally responses with "$nick: passed" when someone wrote "test".
+ * Implements "test" and "ping" commands to test the bot's responsiveness.
  *
  * @category Phergie
  * @package  Phergie_Plugin_PingPong
- * @author   Marcel Glacki <Marcel.Glacki@stud.fh-swf.de>
+ * @author   Phergie Development Team <team@phergie.org>
  * @license  http://phergie.org/license New BSD License
- * @link     http://pear.phergie.org/package/Phergie_Plugin_Pong
- * @link     http://irchelp.org/irchelp/rfc/chapter4.html#c4_6_2
- * @link     http://irchelp.org/irchelp/rfc/chapter4.html#c4_6_3
+ * @link     http://pear.phergie.org/package/Phergie_Plugin_PingPong
  */
 class Phergie_Plugin_PingPong extends Phergie_Plugin_Abstract
 {
-  /**
-   * Responds to "ping" chatmessages.
-   *
-   * @return void
-   */
-  public function onCommandPing( $args = NULL )
-  {
-    // Respond to "ping" only
-    // but not when a message starts with or contains "ping"
-    if( is_null( $args ))
-      $this->doPrivmsg( $this->event->getSource(),
-        $this->event->getNick().": pong" );
-  }
-  /**
-   * Responds to "test" chatmessages.
-   *
-   * @return void
-   */
-  public function onCommandTest( $args = NULL )
-  {
-    // Respond to "test" only
-    // but not when a message starts with or contains "test"
-    if( is_null( $args ))
-      $this->doPrivmsg( $this->event->getSource(),
-        $this->event->getNick().": passed" );
-  }
+    /**
+     * Responds to "ping" messages.
+     *
+     * @return void
+     */
+    public function onCommandPing()
+    {
+        $this->doPrivmsg(
+            $this->event->getSource(),
+            $this->event->getNick() . ': pong'
+        );
+    }
+    /**
+     * Responds to "test" messages.
+     *
+     * @return void
+     */
+    public function onCommandTest()
+    {
+        $this->doPrivmsg(
+            $this->event->getSource(),
+            $this->event->getNick() . ': passed'
+        );
+    }
 }
